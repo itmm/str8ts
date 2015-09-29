48140c2cbb6cf10e9f0f24212ff1bd37df48564a
Timm Knape <timm@kna-st.de>
2015-09-29 19:52:25 +0200

    Initial Commit

---
+++ str8ts/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  str8ts
+//
+//  Created by Timm on 29.09.15.
+//  Copyright © 2015 Timm Knape Softwaretechnik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
